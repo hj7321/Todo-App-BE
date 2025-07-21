326601940859f23644d5742580a7f37eacc01a6f
Hyeonjin <oboki2001@naver.com>
2025-07-21 17:26:53 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+require("dotenv").config();
+const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("mongouri", MONGODB_URI_PROD);
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", indexRouter);
+
+const mongoURI = MONGODB_URI_PROD;
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("Mongoose connected"))
+  .catch((err) => console.err("DB connection failed", err));
+
+app.listen(5000, () => {
+  console.log("server is on 5000");
+});
---
+++ controller/task.controller.js
@@ -0,0 +1,61 @@
+const Task = require("../model/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save();
+    res.status(200).json({ status: "ok", data: newTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.getTasks = async (req, res) => {
+  try {
+    const taskList = await Task.find({}).select("-__v");
+    res.status(200).json({ status: "ok", data: taskList });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { task, isComplete } = req.body;
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { task, isComplete },
+      { new: true }
+    );
+    if (!updatedTask) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Task not found" });
+    }
+    await updatedTask.save();
+    res.status(200).json({ status: "ok", data: updatedTask });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Task not found" });
+    }
+    res.status(200).json({ status: "ok", data: deletedTask });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+module.exports = taskController;
